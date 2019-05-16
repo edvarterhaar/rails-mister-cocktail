253edec3ee673bd94dc110f31db2ade98f5d9343
edvar ter haar <ter.haar.edvar@gmail.com>
2019-05-16 19:25:31 +0200

    everything

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,32 @@
+class DosesController < ApplicationController
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @cocktail = Cocktail.new
+      render 'cocktails/show'
+    end
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+    # WHY ADDING INGREDIENT_ID? BECAUSE THEY ARE A PAIR?
+  end
+end
